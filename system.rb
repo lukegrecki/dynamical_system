e84a2d7af22d4b435b8a80c8c54ec628ed8511a1
Luke Grecki <lukegrecki@gmail.com>
2011-10-12 03:10:09 -0700

    Added ghost_evolve and fixed_points methods

--- system.rb
+++ system.rb
@@ -4,44 +4,62 @@ class System
   attr_reader :state, :history
 
   def initialize(rule, initial_state = nil)
-    if is_valid?(rule)
+    if is_valid_rule?(rule)
       @rule = rule
       @states = rule.keys.to_set
-      @state = initial_state
-      @history = [initial_state] if @state
+      if is_valid_state?(initial_state)
+        @state = initial_state
+        @history = [initial_state]
+      else
+        raise 'Invalid state'
+      end
     else
-      raise 'Rule is not valid'
+      raise 'Invalid rule'
     end
   end
 
   def state=(new_state)
-    @state = new_state
-    @history = [@state]
+    if is_valid_state?(new_state)
+      @state = new_state
+      @history = [@state]
+    else
+      raise 'Invalid state'
+    end
   end
 
   def evolve(steps = 1)
-    if @state
-      steps.times { @state = @rule[@state]; @history << @state }
-      return @state
+    steps.times { @state = @rule[@state]; @history << @state }
+    return @state
+  end
+
+  def ghost_evolve(ghost_state = @state, steps = 1)
+    if is_valid_state?(ghost_state)
+      steps.times { ghost_state = @rule[ghost_state] }
+      return ghost_state
     else
-      raise 'Current state is undefined'
+      raise 'Invalid ghost state'
     end
   end
 
-  def at_fixed_point?
-    if @state == self.evolve
-      true
+  def is_fixed_point?(ghost_state = @state)
+    if is_valid_state?(ghost_state)
+      return ghost_state == self.ghost_evolve(ghost_state) ? true : false
     else
-      false
+      raise 'Invalid ghost state'
     end
   end
 
-  def is_valid?(rule)
-    if (rule.is_a?(Hash) && rule.keys.to_set == rule.values.to_set)
-      return true
-    else
-      return false
-    end
+  def fixed_points
+    fixed_points = @states.select { |s| is_fixed_point?(s) }.to_set
+  end
+
+  def is_valid_rule?(rule)
+    return (rule.is_a?(Hash) &&
+            rule.keys.to_set == rule.values.to_set) ? true : false
+  end
+
+  def is_valid_state?(state)
+    return @states.include?(state) ? true : false
   end
 
 end
