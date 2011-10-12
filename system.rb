c76b510836c010f6d3650593db81531316e91d2f
Luke Grecki <lukegrecki@gmail.com>
2011-10-12 02:04:21 -0700

    Added history attribute

--- system.rb
+++ system.rb
@@ -1,28 +1,35 @@
 require 'set'
 
 class System
-  attr_accessor :state
+  attr_reader :state, :history
 
   def initialize(rule, initial_state = nil)
     if is_valid?(rule)
       @rule = rule
+      @states = rule.keys.to_set
       @state = initial_state
+      @history = [initial_state] if @state
     else
       raise 'Rule is not valid'
     end
   end
 
+  def state=(new_state)
+    @state = new_state
+    @history = [@state]
+  end
+
   def evolve(steps = 1)
     if @state
-      steps.times { @state, @rule = @rule.call(@state) }
-      return @state, @rule
+      steps.times { @state = @rule[@state]; @history << @state }
+      return @state
     else
       raise 'Current state is undefined'
     end
   end
 
   def is_valid?(rule)
-    if rule.is_a?(Proc)
+    if (rule.is_a?(Hash) && rule.keys.to_set == rule.values.to_set)
       return true
     else
       return false
