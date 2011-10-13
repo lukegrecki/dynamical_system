2ab5f5d9bf4c4af328a11da81edc6a24336a1b71
Luke Grecki <lukegrecki@gmail.com>
2011-10-12 23:53:01 -0700

    Fixed computer filing bug

---
+++ examples.rb
@@ -0,0 +1,33 @@
+require_relative 'system'
+
+# A cyclic system with 3 states
+
+rule = { :s1 => :s2, :s2 => :s3, :s3 => :s1 }
+sys = System.new(rule, :s1)
+
+sys.states      # => [:s1, :s2, :s3]
+sys.evolve!(6)  # => :s1
+sys.history     # => [:s1, :s2, :s3, :s1, :s2, :s3, :s1]
+
+# A system with a cycle and a fixed point
+
+rule = { :s1 => :s2, :s2 => :s1, :s3 => :s3 }
+sys = System.new(rule, :s1)
+
+sys.fixed_points                  # => [:s3]
+sys.is_invariant_set?([:s1, :s2]) # => true
+
+# A system with a single attractor
+
+rule = { :s1 => :s3, :s2 => :s3, :s3 => :s3 }
+sys = System.new(rule, :s1)
+
+sys.orbit!(3)            # => [:s1, :s3, :s3, :s3]
+sys.is_fixed_point?(:s3) # => true
+sys.is_bijective?        # => false
+
+# A random system with 5 states
+
+sys = System.random(5)
+sys.states.size # => 5
+
