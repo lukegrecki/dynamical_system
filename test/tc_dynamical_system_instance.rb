9d53b7c32839b9d31a5347d422e4b5e61f3a7367
Luke Grecki <lukegrecki@gmail.com>
2011-10-17 18:00:22 -0700

    Refactored tests

--- test/tc_dynamical_system_instance.rb
+++ test/tc_dynamical_system_instance.rb
@@ -1,7 +1,7 @@
 require_relative '../dynamical_system'
 require 'test/unit'
 
-class TestDynamicalSystem < Test::Unit::TestCase
+class TestDynamicalSystemInstance < Test::Unit::TestCase
 
   def setup
     @rule = { :s1 => :s2, :s2 => :s1, :s3 => :s3 }
@@ -9,20 +9,6 @@ class TestDynamicalSystem < Test::Unit::TestCase
   end
 
   def teardown
-  end
-
-  def test_is_valid_rule?
-    @rule = 0 #not a Hash
-    assert_equal(DynamicalSystem.is_valid_rule?(@rule), false)
-
-    @rule = { :s1 => :s2, :s3 => :s3 } #undefined on :s2
-    assert_equal(DynamicalSystem.is_valid_rule?(@rule), false)
-  end
-
-  def test_random
-    random_sys = DynamicalSystem.random(5)
-    assert_instance_of(DynamicalSystem, random_sys)
-    assert_equal(5, random_sys.states.size)
   end
 
   def test_is_valid_state?
